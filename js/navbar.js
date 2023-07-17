bb44e69f69d7871b84409104658871530b187683
Salman Sheikh <SalmanSheikh2180@gmail.com>
2023-07-17 13:23:10 -0700

    update navbar

--- js/navbar.js
+++ js/navbar.js
@@ -1,3 +1,25 @@
+// window.addEventListener('DOMContentLoaded', function() {
+//   var toggle = document.getElementById('check');
+//   var menu = document.querySelector('.links');
+//   var links = menu.querySelectorAll('a'); // Get all the links inside the menu
+
+//   toggle.addEventListener('change', function() {
+//     if (toggle.checked) {
+//       menu.classList.add('menu-visible');
+//     } else {
+//       menu.classList.remove('menu-visible');
+//     }
+//   });
+
+//   // Add click event listeners to all the links
+//   links.forEach(function(link) {
+//     link.addEventListener('click', function() {
+//       menu.classList.remove('menu-visible'); 
+//     });
+//   });
+// });
+
+
 window.addEventListener('DOMContentLoaded', function() {
   var toggle = document.getElementById('check');
   var menu = document.querySelector('.links');
@@ -14,10 +36,12 @@ window.addEventListener('DOMContentLoaded', function() {
   // Add click event listeners to all the links
   links.forEach(function(link) {
     link.addEventListener('click', function() {
-      menu.classList.remove('menu-visible'); 
+      menu.classList.remove('menu-visible');
+      toggle.checked = false; // Uncheck the toggler
     });
   });
 });
+
 
 
 
